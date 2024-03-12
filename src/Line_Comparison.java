1753169945df7327713db0a29781611e645483f6
veduji16 <jainved01@gmail.com>
2024-03-12 11:35:22 +0530

    Compare two lines based on the end points to know one line is equal, greater or less than the other line.

--- src/Line_Comparison.java
+++ src/Line_Comparison.java
@@ -15,13 +15,13 @@ public class Line_Comparison {
         double x2 = sc.nextDouble();
         System.out.print("Enter the coordinates of y2: ");
         double y2 = sc.nextDouble();
-        System.out.print("Enter the coordinates of y2: ");
+        System.out.print("Enter the coordinates of x3: ");
         double x3 = sc.nextDouble();
-        System.out.print("Enter the coordinates of y2: ");
+        System.out.print("Enter the coordinates of y3: ");
         double y3 = sc.nextDouble();
-        System.out.print("Enter the coordinates of y2: ");
+        System.out.print("Enter the coordinates of x4: ");
         double x4 = sc.nextDouble();
-        System.out.print("Enter the coordinates of y2: ");
+        System.out.print("Enter the coordinates of y4: ");
         double y4 = sc.nextDouble();
 
         double length_1 = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
@@ -30,10 +30,12 @@ public class Line_Comparison {
         double length_2 = Math.sqrt(Math.pow((x4 - x3), 2) + Math.pow((y4 - y3), 2));
         System.out.println("Length of the line: " + length_2);
 
-        if (String.valueOf(length_1).equals(String.valueOf(length_2))) {
+        if (String.valueOf(length_1).compareTo(String.valueOf(length_2)) == 0) {
             System.out.println("Lines are equal");
+        } else if (String.valueOf(length_1).compareTo(String.valueOf(length_2)) > 0) {
+            System.out.println("Length of Line 1 is greater than length of Line 2");
         } else {
-            System.out.println("Lines are not equal");
+            System.out.println("Length of Line 1 is smaller than length of Line 2");
         }
         sc.close();
     }
