85ea0f6db8cc9f91b7b9f87d2d8d01f0bc6a9a95
veduji16 <jainved01@gmail.com>
2024-03-12 11:31:14 +0530

    Check equality of two lines based on the end points

--- src/Line_Comparison.java
+++ src/Line_Comparison.java
@@ -15,9 +15,26 @@ public class Line_Comparison {
         double x2 = sc.nextDouble();
         System.out.print("Enter the coordinates of y2: ");
         double y2 = sc.nextDouble();
+        System.out.print("Enter the coordinates of y2: ");
+        double x3 = sc.nextDouble();
+        System.out.print("Enter the coordinates of y2: ");
+        double y3 = sc.nextDouble();
+        System.out.print("Enter the coordinates of y2: ");
+        double x4 = sc.nextDouble();
+        System.out.print("Enter the coordinates of y2: ");
+        double y4 = sc.nextDouble();
 
-        double length = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-        System.out.println("Length of the line: " + length);
+        double length_1 = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        System.out.println("Length of the line: " + length_1);
+
+        double length_2 = Math.sqrt(Math.pow((x4 - x3), 2) + Math.pow((y4 - y3), 2));
+        System.out.println("Length of the line: " + length_2);
+
+        if (String.valueOf(length_1).equals(String.valueOf(length_2))) {
+            System.out.println("Lines are equal");
+        } else {
+            System.out.println("Lines are not equal");
+        }
         sc.close();
     }
 }
